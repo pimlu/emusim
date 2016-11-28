b4d36da82f31132a8b2aa50bcee355dbb936fff5
Seefo <robbie.uvanni@gmail.com>
2016-11-27 21:55:37 -0500

    Added initial DCPU-16 emulator instruction decoding

--- emu/emuprocess.h
+++ emu/emuprocess.h
@@ -9,13 +9,56 @@ using namespace sim;
 
 class EmuProcess : public Process
 {
-public:
-    EmuProcess(char *ram, int len);
-    Syscall* run(int &c, Sysres *res) override;
-    char *ram;
-    Type lastCall = Type::NONE;
-};
+    static const int WORD_SIZE = 2;
+    static const int BITS_PER_BYTE = 8;
 
+    static const int  WORD_OPCODE_MASK = 0x1F;
+    static const char   WORD_OPCODE_LENGTH = 5;
+
+    static const int  WORD_B_MASK = WORD_OPCODE_MASK << WORD_OPCODE_LENGTH;
+    static const char   WORD_B_LENGTH = 5;
+
+    static const int  WORD_A_MASK = 0xFFFF & ~(WORD_OPCODE_MASK | WORD_B_MASK);
+    static const char   WORD_A_LENGTH = 6;
+
+    enum Opcodes
+    {
+        SET = 0x01,
+        ADD = 0x02,
+        SUB = 0x03,
+        MUL = 0x04,
+        MLI = 0x05,
+        DIV = 0x06,
+        DVI = 0x07,
+        MOD = 0x08,
+        MDI = 0x09,
+        AND = 0x0A,
+        BOR = 0x0B,
+        XOR = 0x0C,
+        SHR = 0x0D,
+        ASR = 0x0E,
+        SHL = 0x0F
+    };
+
+    enum SpecialOpcodes
+    {
+        INT = 0x08
+    };
+
+    struct Registers
+    {
+        short A, B, C, X, Y, Z, I, J;
+        short PC, SP;
+        short EX, IA;
+    };
+
+    public:
+        EmuProcess(char *ram, int len);
+        Syscall* run(int &c, Sysres *res) override;
+        short *ram;
+        Registers registers = {};
+        Type lastCall = Type::NONE;
+    };
 }
 
 #endif // EMUPROCESS_H
