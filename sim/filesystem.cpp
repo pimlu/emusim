26dbc1f42bc0cb3da8c80e7cd224aed1d7e8ee60
changp2 <changp2@mymail.vcu.edu>
2016-11-29 22:36:21 -0500

    filesystem work

--- sim/filesystem.cpp
+++ sim/filesystem.cpp
@@ -1,5 +1,34 @@
 #include "filesystem.h"
+#include <fstream>
+#include <iostream>
+
+
 
 namespace sim {
+    FileSystem::FileSystem(string path) : path(path){
 
+    }
+
+    char* FileSystem:: readFile(string name, int seek, int len){
+        std::ifstream infile(((path + "/" + name).c_str()), std::ifstream::binary);
+        infile.seekg(seek);
+
+        char* buffer= new char[len];
+        infile.read(buffer, len);
+        return buffer;
+
+    }
+
+    bool FileSystem::writeFile(string name, int seek, int len, char *data){
+      std::ofstream outfile((path + "/" + name).c_str(), std::ofstream::binary);
+      outfile.seekp(seek);
+      outfile.write(data,len);
+      return !outfile.bad();
+
+    }
+
+    bool FileSystem::deleteFile(string name){
+       return(remove((path + "/" + name).c_str())==0);
+       
+   }
 }
